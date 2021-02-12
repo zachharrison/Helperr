baad9e97f5311c508ae009ec5d977763383f862a
Adam <adam.tranquilla@gmail.com>
2021-02-12 15:28:45 -0700

    db schema initilized, seeds started

--- db/schema/allseeds.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (username, /* avatar, */email, password )
-VALUES ()
-('BobTheBuilder', 'Bob@BobsBuilding.com', 'password'),
-('Adam', 'adam.tranquilla@gmail.com', 'password'),
-('Natasha', 'natasha@gmail.com', 'password'),
-('Zach', 'zach@gmail.com', 'password');
-
-
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO labber;
