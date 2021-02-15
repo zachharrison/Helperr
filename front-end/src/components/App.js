80d2dabc753d2c0531cf161f03a45cc8686952a1
Zach Harrison <zharrison100@gmail.com>
2021-02-15 00:45:46 +0000

    Created back end express server and implemented chat feature using web sockets

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,9 +1,36 @@
+import React, { useEffect, useRef, useState } from 'react';
 import Navbar from "./navbar/Navbar";
 import Map from "./Map";
 import { Input } from "@material-ui/core";
+import { io } from 'socket.io-client'
 import Jobs from "./Jobs";
+import Chat from "./Chat/Chat";
+const _socket = io.connect('http://localhost:8080', { transports: ["websocket"] });
+
+const useChatSocket = () => {
+  const [messages, setMessages] = useState([]);
+  const socketRef = useRef(_socket);
+  const socket = socketRef.current;
+
+  const sendMessage = ({ name, message }) => {
+    return socket.emit('message', { name, message });
+  };
+
+  useEffect(() => {
+    socket.removeAllListeners();
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  });
+  return { messages, sendMessage};
+};
+
+
 
 function App() {
+
+  const { messages, sendMessage } = useChatSocket();
+
   return (
     <div className="App">
       <Navbar />
@@ -15,6 +42,7 @@ function App() {
 
         <div className="jobs-container">
           <Jobs />
+          <Chat messages={messages} sendMessage={sendMessage}/>
         </div>
 
       </div>
