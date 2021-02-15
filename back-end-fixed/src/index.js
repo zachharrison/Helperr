e49c04632fba48ef6e01cdc2f6e4ee9678cad864
Adam <adam.tranquilla@gmail.com>
2021-02-15 13:14:23 -0700

    backeds merged and sorta working

--- back-end-fixed/src/index.js
+++ back-end-fixed/src/index.js
@@ -1,35 +1,34 @@
 const PORT = process.env.PORT || 8001;
 const ENV = require("./environment");
 
-const app = require("./application")(ENV, { updateAppointment });
+const app = require("./application");
 const server = require("http").Server(app);
+const express = require('express')()
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
 
-wss.on("connection", socket => {
-  socket.onmessage = event => {
-    console.log(`Message Received: ${event.data}`);
+const io = require('socket.io')(server);
+const cors = require('cors');
+const index = require("./routes/index");
 
-    if (event.data === "ping") {
-      socket.send(JSON.stringify("pong"));
-    }
-  };
-});
+let sockets = [];
+express.use(index);
+express.use(cors());
 
-function updateAppointment(id, interview) {
-  wss.clients.forEach(function eachClient(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(
-        JSON.stringify({
-          type: "SET_INTERVIEW",
-          id,
-          interview
-        })
-      );
-    }
+io.on("connection", socket => {
+  sockets.push(socket);
+  console.log(`Client Connected, there is ${sockets.length} sockets connected`);
+  
+  socket.on('disconnect', () => {
+    sockets = sockets.filter(s => s !== socket);
+    console.log(`Client Disconnected, there are ${sockets.length} sockets remaining`);
   });
-}
+  
+  socket.on('message', ({ name, message }) => {
+    sockets.forEach(s => {
+      s.emit('message', { name, message });
+    });
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
