2d4e71efa7b66a8b6facfcd52c247c265b05dedc
Adam <adam.tranquilla@gmail.com>
2021-02-15 22:48:55 -0700

    readme updated

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -6,16 +6,9 @@ import { io } from "socket.io-client";
 import Jobs from "./Jobs";
 import Chat from "./Chat/Chat";
 import fixtures from "./helpers/__mocks__/axios";
-import ChatList from './Chat/ChatList'
+import ChatList from "./Chat/ChatList";
 import useAppData from "./helpers/hooks/useAppData";
-<<<<<<< HEAD
-import { Input } from "@material-ui/core";
-import Chat from "./Chat/Chat";
-import fixtures from "./helpers/__mocks__/axios";
-=======
 // import { Input } from "@material-ui/core";
-// import fixtures from "./helpers/__mocks__/axios";
->>>>>>> ed666a5bd50683bf5f3694d8e3b0dd1df4826b1b
 const _socket = io.connect("http://localhost:8001", {
   transports: ["websocket"],
 });
